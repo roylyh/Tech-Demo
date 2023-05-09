c0301c3bc0628a1047105795ea3ed937b6c89380
roylyh <roy.louyihui@gmail.com>
2023-05-08 23:41:02 -0700

    Navbar

--- react-mui-demo/src/App.js
+++ react-mui-demo/src/App.js
@@ -1,11 +1,13 @@
 import { Box, Stack } from "@mui/material";
 import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Box>
+      <Navbar />
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Feed />
--- react-mui-demo/src/components/Navbar.jsx
+++ react-mui-demo/src/components/Navbar.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Avatar,
+} from "@mui/material";
+import { Mail, Pets, Notifications } from "@mui/icons-material";
+import React from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+// Default theme viewer https://mui.com/material-ui/customization/default-theme/
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: { display: "none" },
+}));
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          ROY DEV
+        </Typography>
+        <Pets sx={{ display: { sm: "none", xs: "block" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+        </Icons>
+        <UserBox>
+          <Avatar sx={{width: 30, height: 30}} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          <Typography variant="span">ROY</Typography>
+        </UserBox>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
