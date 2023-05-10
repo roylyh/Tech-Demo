2396edcef49bf978fd81cee1e95c6edf1b14e353
roylyh <roy.louyihui@gmail.com>
2023-05-09 23:31:45 -0700

    SideBar update

--- react-mui-demo/src/components/Feed.jsx
+++ react-mui-demo/src/components/Feed.jsx
@@ -1,12 +1,20 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </Box>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
